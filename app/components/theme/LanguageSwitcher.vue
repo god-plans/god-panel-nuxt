fb8f2773e6863c607891ba59bdd29d882e90e791
parsajiravand <parsajiravand@gmail.com>
2025-11-13 15:20:38 +0330

    Fix responsive header and default lang

--- app/components/theme/LanguageSwitcher.vue
+++ app/components/theme/LanguageSwitcher.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { useLocale } from "vuetify";
 
+// Import SVG icons
+const usFlagIcon = "/assets/icons/us-flag.svg";
+const iranFlagIcon = "/assets/icons/iran-flag.svg";
 
 const { t, setLocale, locale } = useI18n();
 const langFromCookie = useCookie("lang");
@@ -9,8 +12,6 @@ const { current } = useLocale();
 function changeLocale(locale) {
   current.value = locale;
 }
-
-
 
 // watch
 watch(
@@ -24,10 +25,9 @@ watch(
   { immediate: false }
 );
 
-function set(lang) { 
+function set(lang) {
   // Prevent unnecessary updates if already set
   if (locale.value !== lang) {
-
     setLocale(lang);
     langFromCookie.value = lang;
   }
@@ -37,48 +37,30 @@ function set(lang) {
 <template>
   <v-menu>
     <template v-slot:activator="{ props }">
-      <button type="button" class="flex items-center gap-x-2" v-bind="props">
+      <v-btn
+        type="button"
+        icon
+        class="flex items-center justify-center"
+        v-bind="props"
+      >
         <div v-if="locale === 'en'" class="flex items-center gap-x-2">
-        <v-icon icon="mdi-united-states" />
-          
-          <p
-           
-            class="text-sm  lg:block hidden"
-          >
-            English (US)
-          </p>
+          <img :src="usFlagIcon" alt="US Flag" class="w-5 h-5" />
         </div>
         <div v-else class="flex items-center gap-x-2">
-          <v-icon icon="mdi-iran" />
-          <p
-           
-            class="text-sm  lg:block hidden"
-          >
-            Farsi (IR)
-          </p>
+          <img :src="iranFlagIcon" alt="Iran Flag" class="w-5 h-5" />
         </div>
-        <Icon
-         
-          name="tabler:chevron-down"
-          class="h-5 w-5 lg:!block !hidden"
-        />
-      </button>
+
+      </v-btn>
     </template>
     <section
       class="flex flex-col justify-start items-start gap-2 sm:gap-4 bg-white dark:!bg-gray-900 p-4 rounded-xl mt-3 border-1 border-solid border-neutral-100 dark:!border-neutral-400"
     >
-      <button
-        class="flex items-center gap-x-1 "
-        @click="set('en')"
-      >
-        <v-icon icon="mdi-united-states" />
+      <button class="flex items-center gap-x-1" @click="set('en')">
+        <img :src="usFlagIcon" alt="US Flag" class="w-5 h-5" />
         <p>English (US)</p>
       </button>
-      <button
-        @click="set('fa')"
-        class="flex items-center gap-x-1 "
-      >
-        <v-icon icon="mdi-iran" />
+      <button @click="set('fa')" class="flex items-center gap-x-1">
+        <img :src="iranFlagIcon" alt="Iran Flag" class="w-5 h-5" />
         <p>Farsi (IR)</p>
       </button>
     </section>
